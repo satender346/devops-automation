a3f65b0cc86242a8e0badfef6dbc0cef60ff22a6
Satender Yadav <satender346@gmail.com>
2021-09-18 17:03:14 -0500

    Update packer.sh

--- packer.sh
+++ packer.sh
@@ -50,4 +50,4 @@ sudo mkdir -p /var/jenkins_home
 
 sudo chmod 777 /var/jenkins_home
 sudo docker pull jenkins/jenkins
-sudo docker run --restart=always -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home jenkins/jenkins
+sudo docker run -d --restart=always -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home jenkins/jenkins
